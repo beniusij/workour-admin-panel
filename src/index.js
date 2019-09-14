02227ae44b631fbe5f78e8dca0638e7949937d90
Juozas Beniusis <beniusij@gmail.com>
2019-09-14 01:01:32 +0100

    Shift around boilerplate css and custom typography

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import './index.css'
+import './styles/skeleton.css'
+import './styles/normalize.css'
+import './App.scss'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import './index.css';
-import App from './App';
+import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(

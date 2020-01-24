570c071c7023b78729757e8df31323771382f11c
Juozas Joe Beniusis <jb@bluetel.co.uk>
2020-01-24 22:47:29 +0000

    Update yarn dependencies

---
+++ src/components/navComponents/ProtectedRoute.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {AuthConsumer} from "../../context/Auth/AuthConsumer";
+import {Redirect, Route} from "react-router-dom";
+
+function  ProtectedRoute({children, ...rest}) {
+  return (
+    <AuthConsumer>
+      {({ user }) => (
+        <Route
+          {...rest}
+          render={({ location }) =>
+            user.isAuth ? (
+              location
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/signin",
+                  state: { from: location }
+                }}
+              />
+            )
+          }
+        />
+      )}
+    </AuthConsumer>
+  )
+}
+
+export default ProtectedRoute

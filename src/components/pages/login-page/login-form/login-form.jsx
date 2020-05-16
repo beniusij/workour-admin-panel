af1617c381ef449abafbe570580f97172a15819b
Juozas Joe Beniusis <jb@bluetel.co.uk>
2020-05-16 21:37:34 +0100

    Update login form to use Form component

--- src/components/pages/login-page/login-form/login-form.jsx
+++ src/components/pages/login-page/login-form/login-form.jsx
@@ -1,34 +1,28 @@
 import React from 'react'
 import Button from '@bit/beniusij.ourworkout.ui-components.button'
 import InputField from '@bit/beniusij.ourworkout.input-field'
-import styles from './login-form.module.scss'
-import Notification from "@bit/beniusij.ourworkout.notification";
+import Form from '../../../ui-components/form/form'
 import {AuthConsumer} from "../../../context/auth-context/auth-consumer";
 
 function LoginForm() {
   return (
     <AuthConsumer>
       {({error, authenticate, loading}) => (
-        <div className={styles.formContainer} >
-          <h2 className={styles.formTitle}>Log In</h2>
-
-          {
-            error &&
-            <Notification type={"failure"} message={error} />
-          }
-
-          <form onSubmit={authenticate}>
-            <InputField
-              name="Email"
-              type="email"
-            />
-            <InputField
-              name="Password"
-              type="password"
-            />
-            <Button type={'submit'} text={loading ? 'Loading...' : 'Sign In'} />
-          </form>
-        </div>
+        <Form
+          title={'Login'}
+          error={error}
+          formFunction={authenticate}
+        >
+          <InputField
+            name="Email"
+            type="email"
+          />
+          <InputField
+            name="Password"
+            type="password"
+          />
+          <Button type={'submit'} text={loading ? 'Loading...' : 'Sign In'} />
+        </Form>
       )}
     </AuthConsumer>
   )

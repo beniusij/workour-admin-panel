4e8b66a63c4a9760710f0632c853e615b2992b0c
Juozas Joe Beniusis <jb@bluetel.co.uk>
2020-05-26 11:55:22 +0100

    Update burger menu

--- src/components/navigation/nav/nav.jsx
+++ src/components/navigation/nav/nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavLink from 'components/navigation/nav-link/nav-link.jsx'
 import styles from './nav.module.scss'
 import { AuthConsumer } from "components/context/auth-context/auth-context"
@@ -6,20 +6,50 @@ import NavButton from "components/navigation/nav-button/nav-button";
 import Logo from "components/ui-components/logo/logo";
 
 function Navigation() {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
+  let topBar = showMenu
+    ? `${styles.burgerBarTop} ${styles.burgerBarTopToggled}`
+    : styles.burgerBarTop
+
+  let middleBar = showMenu
+    ? styles.burgerBarMiddleToggled
+    : styles.burgerBarMiddle
+
+  let bottomBar = showMenu
+    ? `${styles.burgerBarBottom} ${styles.burgerBarBottomToggled}`
+    : styles.burgerBarBottom
+
   return (
     <AuthConsumer>
       {({ logout }) => (
         <nav className={styles.navContainer}>
-          <Logo/>
-          <NavLink to="/">
-            Home
-          </NavLink>
-          <NavLink to={"/data"}>
-            Data
-          </NavLink>
-          <NavButton eventFunction={logout}>
-            Sign out
-          </NavButton>
+          <div className={styles.controlsGroup}>
+            <Logo/>
+
+            <div onClick={toggleMenu} className={styles.burgerButton}>
+              <div className={topBar}></div>
+              <div className={middleBar}></div>
+              <div className={bottomBar}></div>
+            </div>
+          </div>
+
+          <div className={styles.linksGroup}>
+            <NavLink to="/">
+              Home
+            </NavLink>
+            <NavLink to={"/data"}>
+              Data
+            </NavLink>
+            <NavButton eventFunction={logout}>
+              Sign out
+            </NavButton>
+          </div>
+
         </nav>
       )}
     </AuthConsumer>

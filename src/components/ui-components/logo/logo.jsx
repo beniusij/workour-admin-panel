16ab4c66f387e3c1e6a06f2ebe3b3a16b0577feb
Juozas Joe Beniusis <jb@bluetel.co.uk>
2020-05-26 09:47:22 +0100

    Add logo

--- src/components/ui-components/logo/logo.jsx
+++ src/components/ui-components/logo/logo.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import BrandLogo from './dumbbell-solid.svg'
+import styles from './logo.module.scss'
+import {Link} from "react-router-dom";
 
 /**
  * Brand logo usually used only in navigation bar
@@ -8,8 +10,9 @@ import BrandLogo from './dumbbell-solid.svg'
  * @constructor
  */
 export default function Logo() {
-  console.log(BrandLogo)
   return (
-    <img src={BrandLogo} alt={"Brand logo"} />
+    <Link className={styles.logoLink} to={"/"}>
+      <img className={styles.logoImg} src={BrandLogo} alt={"OurWorkout Logo"} />
+    </Link>
   )
 }
